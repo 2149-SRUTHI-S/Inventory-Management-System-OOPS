3c0ed51f99cc36f89dcfa686cd09f027b22addcb
Sruthi Sivasankararaj <129876043+2149-SRUTHI-S@users.noreply.github.com>
2025-02-28 10:42:06 +0530

    Create RecipeMgmt.java

---
+++ RecipeMgmt.java
@@ -0,0 +1,19 @@
+package myInventory;
+
+import java.util.ArrayList;
+
+public class RecipeMgmt {
+	public String recipe_name;
+	public ArrayList<String> ing;
+	
+	RecipeMgmt(String recipe_name, ArrayList<String> ing)
+	{
+		this.recipe_name = recipe_name;
+		this.ing = ing;
+	}
+	
+	public void display()
+	{
+		
+	}
+}
